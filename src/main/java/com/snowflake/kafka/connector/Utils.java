4c37a0c94f40a0d0fb106bed0c674510587f975f
Andrew Wang <andrew.wang@invitae.com>
2020-12-15 10:23:32 -0800

    upversion to 2.6.0 (#14)

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -44,7 +44,7 @@ public class Utils
 {
 
   //Connector version, change every release
-  public static final String VERSION = "2.5.0";
+  public static final String VERSION = "2.6.0";
 
   //connector parameter list
   public static final String NAME = "name";
